25ffa08e4337644b8fa038506f33e3eef41a1fca
cikabole <cikabole@gmail.com>
2015-01-10 12:05:21 +0100

    Initial Commit

---
+++ Checkin/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  Checkin
+//
+//  Created by Borislav Jagodic on 1/10/15.
+//  Copyright (c) 2015 Krooya. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
